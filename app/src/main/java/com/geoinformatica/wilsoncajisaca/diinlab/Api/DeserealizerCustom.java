024c96191c6d93f25415fa942f60a70a58e76eb7
wilsoncajisaca <wilsoncajisaca@gmail.com>
2019-04-15 13:00:53 -0500

    Commit

--- app/src/main/java/com/geoinformatica/wilsoncajisaca/diinlab/Api/DeserealizerCustom.java
+++ app/src/main/java/com/geoinformatica/wilsoncajisaca/diinlab/Api/DeserealizerCustom.java
@@ -1,4 +1,26 @@
 package com.geoinformatica.wilsoncajisaca.diinlab.Api;
 
-public class DeserealizerCustom {
+import com.geoinformatica.wilsoncajisaca.diinlab.Common.Events;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+
+import java.lang.reflect.Type;
+
+public class DeserealizerCustom implements JsonDeserializer<Events> {
+
+    @Override
+    public Events deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+
+        String id = json.getAsJsonObject().get("datos").getAsJsonObject().get("id").getAsString();
+        String allday=json.getAsJsonObject().get("datos").getAsJsonObject().get("allday").getAsString();
+        String titulo=json.getAsJsonObject().get("datos").getAsJsonObject().get("titulo").getAsString();
+        String startime=json.getAsJsonObject().get("datos").getAsJsonObject().get("startime").getAsString();
+        String endtime=json.getAsJsonObject().get("datos").getAsJsonObject().get("endtime").getAsString();
+        String lugar=json.getAsJsonObject().get("datos").getAsJsonObject().get("lugar").getAsString();
+
+        Events events=new Events(id,allday,titulo,startime,endtime,lugar);
+        return events;
+    }
 }
